97eca520189689eddc16c0804609ff44339990c0
Brant McCarthy <brant.mccarthy@me.com>
2022-01-29 15:29:56 -0600

    added currency + decimal styling

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 $(readyNow);
 
 let employees = [];
+const monthlyTotalMax = 20000;
 
 function readyNow(){
     $( '#btnSubmit' ).on('click', handleSubmit);
+    $( 'table' ).on('click', '#btnDelete', handleDelete);
 }
 
 function handleSubmit(){
@@ -14,7 +16,8 @@ function handleSubmit(){
     lastName = $( '#last-name' ).val();
     idNum = $( '#id-num' ).val();
     jobTitle = $( '#job-title' ).val();
-    annSalary = Number(Math.round(($( '#ann-salary' ).val())+'e'+2)+'e-'+2); // Rounding
+    // Rounding Equation
+    annSalary = Number(Math.round(($( '#ann-salary' ).val())+'e'+2)+'e-'+2); 
 
     // Alert if missing input info
     // if (
@@ -31,7 +34,7 @@ function handleSubmit(){
     // empties input fields
     $( 'input' ).val( '' );
 
-    // create array object from inputs
+    // create array-object from input values
     employees.push({
         firstName: firstName,
         lastName: lastName,
@@ -40,33 +43,67 @@ function handleSubmit(){
         annSalary: annSalary,
     })
 
-    // empty tbody before re-running function
+    // empty tbody 
     $( 'tbody' ).empty();
 
     // Creates new row in table with object array info
-    // Also calculates annualTotal
-    let annSalaryTotal = 0;
     for (let employee of employees){
         $( 'tbody' ).append(`<tr>
             <td>${employee.firstName}</td>
             <td>${employee.lastName}</td>
             <td>${employee.idNum}</td>
             <td>${employee.jobTitle}</td>
-            <td>$${employee.annSalary}</td>
-            <td><button class="btnDelete">Delete</button></td>
+            <td>$${(employee.annSalary).toLocaleString("en-US")}</td>
+            <td><button id="btnDelete">Delete</button></td>
         </tr>`); 
+    }
+    calcMonthlyTotal(); 
 
+} // end handleSubmit
+
+function handleDelete(){
+
+    let i = this.parentNode.parentNode.rowIndex;
+    document.getElementById("employee-table").deleteRow(i);
+    employees.splice( i-1, 1);
+    calcMonthlyTotal();
+    checkMonthlyMax();
+
+} // end handleDelete
+
+function calcMonthlyTotal(){
+
+    // Calculate Monthly total w/ Rounding
+    let annSalaryTotal = 0;
+    for (let employee of employees){
         annSalaryTotal += Number(employee.annSalary);
     }
-    // Divides annSalaryTotal by 12 (monthly),
-    monthlyTotal = Number(Math.round((annSalaryTotal / 12)+'e'+2)+'e-'+2); // Rounds
 
-    // Empty + append the Monthly total
-    $( '#totalMonthly' ).empty();
-    $( '#totalMonthly' ).append(monthlyTotal);
-    if (monthlyTotal > 20000){
-        $( '.totalMonthlyContainer' ).css('background-color', 'red')
-    }
+    console.log('annSalaryTotal:', annSalaryTotal);
+    let monthlyTotalRounded = Number(Math.round((annSalaryTotal / 12)+'e'+2)+'e-'+2);
+    console.log('monthlyTotalRounded', monthlyTotalRounded);
+    let monthlyTotalDecimals = Number(monthlyTotalRounded.toFixed(2)); // add float: 0.00
+    console.log('monthlyTotalDecimals:', monthlyTotalDecimals);  
+    let monthlyTotalCommas = monthlyTotalDecimals.toLocaleString("en-US"); // add commas: 1,000
+    console.log('monthlyTotalCommas:', monthlyTotalCommas);
     
-}
 
+    // Empty + append the Monthly total to DOM
+    $( '#totalMonthly' ).empty();
+    $( '#totalMonthly' ).append(monthlyTotalCommas);
+    
+    checkMonthlyMax(monthlyTotalRounded);
+
+} // end calcMonthlyTotal
+
+function checkMonthlyMax( monthlyTotalRounded ){
+    
+    // enables red background color
+    let totalMonthlyContainer = $( '.totalMonthlyContainer' );
+    if (monthlyTotalRounded > monthlyTotalMax){
+        totalMonthlyContainer.css('background-color', 'red')
+    } else {
+        totalMonthlyContainer.css('background-color', 'white')
+    }
+
+} // end checkMonthlyMax
